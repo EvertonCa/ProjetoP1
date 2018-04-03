3e00eb99ca745819075b7a6a3de678a239cb83c5
EvertonCa <eve.023@hotmail.com>
2018-04-03 03:51:42 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main (String argv[])
+    {
+        Scanner teclado = new Scanner(System.in);
+    }
+}
---
+++ src/Veiculo.java
@@ -0,0 +1,10 @@
+public class Veiculo
+{
+    public void move()
+    {
+
+    }
+    protected int x, y, velocidade;
+    protected boolean fabrica;
+    protected String cor;
+}
