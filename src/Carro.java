8879c2a72f4746ba2a44baadd33628445420c265
Everton Cardoso Acchetta <eve.023@hotmail.com>
2018-04-12 15:14:30 -0300

    Comentários adicionados

--- src/Carro.java
+++ src/Carro.java
@@ -1,10 +1,10 @@
 public class Carro extends Veiculo
 {
-    public Carro()
+    public Carro() //construtor padrão com número de passageiros aleatório
     {
-        super(2, "azul");
+        super(2, "azul"); //passa dados para o construtor padrão de veiculos
         Aleatorio random = new Aleatorio();
-        numPassageiros = random.randomizePassageiros();
+        numPassageiros = random.randomizePassageiros(); //
     }
 
     protected int numPassageiros;
