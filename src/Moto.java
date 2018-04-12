a65d949369da02c862e1400e271c54480bdc243e
Everton Cardoso Acchetta <eve.023@hotmail.com>
2018-04-12 14:52:05 -0300

    Comentários adicionados

--- src/Moto.java
+++ src/Moto.java
@@ -1,8 +1,8 @@
 public class Moto extends Veiculo
 {
-    public Moto()
+    public Moto() //construtor padrão com tipo da moto aleatória
     {
-        super(3, "marrom");
+        super(3, "marrom"); //chama construtor padrão da classe pai
         Aleatorio random = new Aleatorio();
         tipo = random.randomizeTipo();
     }
