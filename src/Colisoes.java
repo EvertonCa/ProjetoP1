76f657773741c2bea1a8f13d9aaaa9ce202742f0
Everton Cardoso Acchetta <eve.023@hotmail.com>
2018-04-12 15:12:55 -0300

    Comentários adicionados

--- src/Colisoes.java
+++ src/Colisoes.java
@@ -1,11 +1,11 @@
 public class Colisoes
 {
-    public Colisoes()
+    public Colisoes() //construtor padrão
     {
-        tipos = new String[30][60];
-        id = new int[30][60];
+        tipos = new String[30][60]; //nova matriz de 30x60 para armazenar os tipos de veiculos
+        id = new int[30][60]; //nova matriz de 30x60 para armazenar as IDs dos veiculos
 
-        for (int i = 0; i < 30; i++)
+        for (int i = 0; i < 30; i++) //inicializa as duas matrizes como vazias
         {
             for (int j = 0; j < 60; j++)
             {
@@ -15,23 +15,23 @@ public class Colisoes
         }
     }
 
-    public String getTipo(int i, int j)
+    public String getTipo(int i, int j) //retorna o tipo que esta nas coordenadas entradas
     {
         return tipos[i][j];
     }
 
-    public int getID(int i, int j)
+    public int getID(int i, int j) //retorna a ID que esta nas coordenadas entradas
     {
         return id[i][j];
     }
 
-    public void ocupado(int i, int j, String tipo, int ID)
+    public void ocupado(int i, int j, String tipo, int ID) // popula a coordenada entrada com o tipo e ID desejado
     {
         tipos[i][j] = tipo;
         id[i][j] = ID;
     }
 
-    public void restaura(int i, int j)
+    public void restaura(int i, int j) //restaura a coordenada entrada como padrão vazio
     {
         tipos[i][j] = "empty";
         id[i][j] = 0;
